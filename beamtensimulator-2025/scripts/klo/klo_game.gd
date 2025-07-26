e53ae8efe482915a85ac21f7749248662fbf1e9b
tobstar97 <tobi.tobstar97@gmail.com>
2025-07-26 23:55:07 +0200

    klo funktioniert

---
+++ beamtensimulator-2025/scripts/klo/klo_game.gd
@@ -0,0 +1,83 @@
+extends Node2D
+
+# === NODES ===
+@onready var background       = $TextureRect
+@onready var input_coffee     = $LineEdit
+@onready var button_start     = $Button
+@onready var label_instr      = $LabelInstruction
+@onready var label_timer      = $LabelTimer
+@onready var timer_session    = $TimerSession
+@onready var audio_toilet     = $AudioStreamPlayer
+
+# === KONSTANTEN ===
+const TIME_PER_COFFEE = 5.0  # Sekunden pro Kaffee
+const OFFICE_SCENE    = "res://scenes/main_office.tscn"  # Pfad zur Büroszene
+
+# === SPIELSTATUS ===
+var total_time      = 0.0
+var session_finished = false
+
+func _ready():
+	# Initial-UI
+	label_instr.visible    = true
+	label_timer.visible    = false
+	label_instr.text       = "Wie viele Kaffees hast du getrunken?"
+	button_start.disabled  = false
+	input_coffee.editable  = true
+	session_finished       = false
+
+	# Signale verbinden
+	button_start.pressed.connect(_on_Button_start)
+	timer_session.timeout.connect(_on_Timer_timeout)
+
+	# Toilet-Audio pausieren
+	audio_toilet.stream_paused = true
+
+func _on_Button_start() -> void:
+	var coffees = int(input_coffee.text)
+	if coffees < 1:
+		label_instr.text = "Bitte eine positive Zahl eingeben!"
+		return
+
+	# Berechne gesamte Sitzdauer
+	total_time = coffees * TIME_PER_COFFEE
+
+	# UI umschalten
+	label_instr.visible    = false
+	input_coffee.visible   = false
+	button_start.visible   = false
+	label_timer.visible    = true
+	session_finished       = false
+
+	# Timer starten
+	timer_session.wait_time = total_time
+	timer_session.one_shot  = true
+	timer_session.start()
+
+	# Toilet-Audio starten
+	audio_toilet.stream_paused = false
+	audio_toilet.play()
+
+	set_process(true)
+
+func _process(delta: float) -> void:
+	if timer_session.time_left > 0:
+		var t = timer_session.time_left
+		var display = round(t * 10) / 10.0
+		label_timer.text = str(display) + "s verbleibend"
+	elif not session_finished:
+		# Übergang in Fertig-Zustand
+		label_timer.text = "Fertig! 🚽\nDrücke ESC zum Büro"
+		session_finished = true
+		set_process(false)
+
+func _on_Timer_timeout() -> void:
+	# Toilet-Audio stoppen
+	audio_toilet.stop()
+	audio_toilet.stream_paused = true
+	# _process() kümmert sich um das Fertig-Label
+
+func _input(event):
+	# Nur nach Ende der Sitzung auf ESC reagieren
+	if session_finished and event.is_action_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://scenes/main_office.tscn")
--- beamtensimulator-2025/scripts/nap/nap_game.gd
+++ beamtensimulator-2025/scripts/nap/nap_game.gd
@@ -1,56 +1,82 @@
 extends Node2D
 
-@onready var bg_normal = $BackgroundNormal
-@onready var bg_boss = $BackgroundWithBoss
-@onready var label_status = $StatusLabel
-@onready var timer_boss = $TimerBoss
-@onready var audio_steps = $AudioBossSteps
+# === NODES ===
+@onready var bg_normal      = $BackgroundNormal
+@onready var bg_boss        = $BackgroundWithBoss
+@onready var label_status   = $StatusLabel
+@onready var timer_boss     = $TimerBoss
+@onready var timer_footstep = $TimerFootstep
+@onready var audio_steps    = $AudioBossSteps
 
-var sleeping = false
-var caught = false
+# === SPIELSTATUS ===
 var sleep_time = 0.0
+var caught     = false
 
 func _ready():
 	randomize()
 	bg_normal.visible = true
-	bg_boss.visible = false
+	bg_boss.visible   = false
 	label_status.text = ""
-	timer_boss.timeout.connect(_on_TimerBoss_timeout)
-	timer_boss.wait_time = randf_range(3.0, 15.0)
-	timer_boss.start()
+	caught = false
+	sleep_time = 0.0
 
+	# Signale verbinden
+	timer_boss.timeout.connect(_on_TimerBoss_timeout)
+	timer_footstep.timeout.connect(_on_TimerFootstep_timeout)
+
+	# Zufällige Zeit bis Boss-Auftritt (3–15 s)
+	var wait_time = randf_range(3.0, 5.0)
+	timer_boss.wait_time     = wait_time
+	timer_footstep.wait_time = max(wait_time - 2.0, 0.1)
+
+	timer_boss.start()
+	timer_footstep.start()
+	set_process(true)
 
 func _process(delta):
+	var sleeping = Input.is_action_pressed("ui_accept")
 	if sleeping and not caught:
 		sleep_time += delta
 		label_status.text = "Zzz... " + str(round(sleep_time * 10) / 10.0) + "s"
 	elif not caught:
 		label_status.text = "Wachsam!"
 
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		sleeping = true
-	elif event.is_action_released("ui_accept"):
-		sleeping = false
+func _on_TimerFootstep_timeout():
+	# Schritte exakt 2 s vor dem Boss
+	audio_steps.play()
+	await get_tree().create_timer(2.0).timeout
+	audio_steps.stop()
 
 func _on_TimerBoss_timeout():
-	audio_steps.play()
+	# Abfragen, ob aktuell geschlafen wird
+	var sleeping = Input.is_action_pressed("ui_accept")
+
+	# Boss erscheint
 	bg_normal.visible = false
-	bg_boss.visible = true
+	bg_boss.visible   = true
 
 	if sleeping:
 		caught = true
 		label_status.text = "Erwischt!"
 		await get_tree().create_timer(2.0).timeout
 		get_tree().reload_current_scene()
+		return
 	else:
 		label_status.text = "Beinahe erwischt!"
-		await get_tree().create_timer(1.5).timeout
 
-		# Zurück zum normalen Hintergrund
-		bg_boss.visible = false
+		# Boss bleibt 2–4 Sekunden, dann verschwindet er
+		var vanish_time = randf_range(2.0, 4.0)
+		await get_tree().create_timer(vanish_time).timeout
+
+		# Zurück zum normalen Zustand
+		bg_boss.visible   = false
 		bg_normal.visible = true
 		label_status.text = "Wachsam!"
-		
-		timer_boss.wait_time = randf_range(3.0, 8.0)
+		# sleep_time bleibt erhalten!
+
+		# Nächsten Zufallstimer setzen und starten
+		var next_wait = randf_range(3.0, 15.0)
+		timer_boss.wait_time     = next_wait
+		timer_footstep.wait_time = max(next_wait - 2.0, 0.1)
 		timer_boss.start()
+		timer_footstep.start()
